a79b2c9ac47d8e4f7b3c92ce282a35b703cc0644
Galex Yen <galexy@users.noreply.github.com>
2019-10-04 22:59:47 -0700

    Fix incorrect incrementing of columns vs rows during layout

--- src/main/scala/net/reasoning/eqcel/compiler/LayoutPhase.scala
+++ src/main/scala/net/reasoning/eqcel/compiler/LayoutPhase.scala
@@ -8,7 +8,7 @@ object LayoutPhase extends Phase[ReifiedSheet, LayoutSheet] {
   def transform(tree: ReifiedSheet): LayoutSheet = {
     // hard code to one row per range
     val layouts = tree.ranges.zipWithIndex.map {
-      case (range, index) => (range.id, RowLayout((index, 0), range.start, range.end))
+      case (range, index) => (range.id, RowLayout((0, index), range.start, range.end))
     }
 
     val rangeLayouts = RangeLayouts(Map.from(layouts))
